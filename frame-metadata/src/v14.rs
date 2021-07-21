8b9945959f37b4e7489f99b680f70b03202466e8
Andrew Jones <ascjones@gmail.com>
2021-07-21 08:41:09 +0100

    Fmt

--- frame-metadata/src/v14.rs
+++ frame-metadata/src/v14.rs
@@ -265,7 +265,7 @@ impl StorageEntryMetadata<MetaForm> {
 
 impl<T> StorageEntryMetadata<T>
 where
-	T: Form
+	T: Form,
 {
 	/// Get the documentation.
 	pub fn docs(&self) -> &[T::String] {
@@ -452,7 +452,7 @@ impl PalletConstantMetadata {
 
 impl PalletConstantMetadata<T>
 where
-	T: Form
+	T: Form,
 {
 	/// Get the documentation.
 	pub fn docs(&self) -> &[T::String] {
